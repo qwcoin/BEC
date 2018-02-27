179fa6a580145882541228b5f780282ea4819667
lhq <lihuoqin@qq.com>
2018-02-27 13:42:55 +0800

    add geth files

---
+++ build/env.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+if [ ! -f "build/env.sh" ]; then
+    echo "$0 must be run from the root of the repository."
+    exit 2
+fi
+
+# Create fake Go workspace if it doesn't exist yet.
+workspace="$PWD/build/_workspace"
+root="$PWD"
+ethdir="$workspace/src/github.com/ethzero"
+if [ ! -L "$ethdir/go-ethzero" ]; then
+    mkdir -p "$ethdir"
+    cd "$ethdir"
+    ln -s ../../../../../. go-ethzero
+    cd "$root"
+fi
+
+# Set up the environment to use the workspace.
+GOPATH="$workspace"
+export GOPATH
+
+# Run the command inside the workspace.
+cd "$ethdir/go-ethzero"
+PWD="$ethdir/go-ethzero"
+
+# Launch the arguments with the configured environment.
+exec "$@"
